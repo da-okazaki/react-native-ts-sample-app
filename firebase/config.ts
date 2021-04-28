fdd0678f0e2884541e18f302963d4743fd61eda9
da-okazaki <da-okazaki@outlook.com>
2021-04-28 16:47:48 +0900

    [add] firebase introduction.

---
+++ firebase/config.ts
@@ -0,0 +1,9 @@
+/* firebase console copy */
+export const firebaseConfig = {
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
+};
