afc3137dcc983f063d48d2d58a297b26b2189c28
da-okazaki <da-okazaki@outlook.com>
2021-04-28 14:34:22 +0900

    [update] paper theme introduction.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 /* react native */
 import React from 'react';
 /* react native paper */
-import { Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 /* Navigation */
 import Navigation from './navigation';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+}
+
 export default function App() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <Navigation  />;
     </PaperProvider>
   )
